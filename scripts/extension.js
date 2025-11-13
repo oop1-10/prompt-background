8e14bc927f80f56ad4b1d5b2bfea3bae79d58a8e
Thomas Newman <tnewman057@gmail.com>
2025-11-13 14:09:57 -0500

    First Beginnings

---
+++ scripts/extension.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    background = document.querySelector("scrollable-container");
+    background.style.background = 'url(BACKGROUND-IMAGE)';
+});
